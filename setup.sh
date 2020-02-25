656f526edef2693bb79669a55acdb45627ac3094
ihcomega56 <ihcomega56@gmail.com>
2020-02-25 19:51:29 +0900

    add necessary things

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Thank you @laysakura
+function has() {
+      type "$1" >/dev/null 2>&1
+      return $?
+}
+
+has reattach-to-user-namespace || brew install reattach-to-user-namespace
+has peco || brew install peco
+
 DOT_DIRECTORY="$HOME/IdeaProjects/dotfiles"
 
 cd ${DOT_DIRECTORY}
